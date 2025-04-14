15f068e90851dcaeaa1d7a5c966d8ad20c21002b
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-04-14 13:14:23 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,108 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int[] bubbleSort(double[] nums) {
+		boolean swapped;
+		double temp;
+		int arr[] = new int[2];
+		for (int i = 0; i < nums.length - 1; ++i) {
+			swapped = false;
+			for (int j = 0; j < nums.length - 1 - i; ++j) {
+				if (nums[j] > nums[j + 1]) {
+					temp = nums[j + 1];
+					nums[j + 1] = nums[j];
+					nums[j] = temp;
+					arr[0] += 3; // swap done
+					swapped = true;
+				}
+				++arr[1]; // if condition
+			}
+			if (!swapped) break;
+		}
+		return arr;
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static int[] selectionSort (double[] nums) {
+		int lowest;
+		double temp;
+		int arr[] = new int[2];
+		for (int i = 0; i < nums.length - 1; ++i) {
+			lowest = i;
+			// ++arr[0]; // variable reassignment
+			// ^ this isn't included (initialization sorta ig)
+			for (int j = i + 1; j < nums.length; ++j) {
+				if (nums[j] < nums[lowest]) {
+					lowest = j;
+					++arr[0]; // variable reassignment
+				}
+				++arr[1]; // if condition
+			}
+			temp = nums[i];
+			nums[i] = nums[lowest];
+			nums[lowest] = temp;
+			arr[0] += 3; // swap done
+		}
+		return arr; // arr[1] will always be (nums.length)(nums.length - 1)/2
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static int[] insertionSort(double[] nums) {
+		int index;
+		double key;
+		int arr[] = new int[2];
+		for (int i = 1; i < nums.length; ++i) {
+			key = nums[i];
+			index = i - 1;
+			arr[0] += 2; // 2 variable reassignments
+			while (index >= 0 && nums[index] > key) {
+				++arr[1]; // while condition
+				nums[index + 1] = nums[index--];
+				++arr[0]; // variable reassignment
+			}
+			if (index == i - 1) ++arr[1]; // if broken out of the while loop based on comparison SPECIFICALLY when the element is already sorted (edge case, fine)
+			nums[index + 1] = key;
+			// ++arr[0]; // variable reassignment
+			// ^ this isn't included?????????
+		}
+		return arr;
 	}
 
-	public static void q3() {
-		//Write question 3 code here
+	public static String leastSwaps(double[] nums) {
+		int[] swapNum = new int[3]; // stores number of swaps for bubble, insertion, selection sort in that order
+		String[] sortNames = {"Bubble", "Insertion", "Selection"}; // parallel array to store the names of each algorithm
+		double[] numsCopy = new double[nums.length];
+		for (int i = 0; i < nums.length; ++i) numsCopy[i] = nums[i];
+		swapNum[0] = bubbleSort(numsCopy)[0];
+		for (int i = 0; i < nums.length; ++i) numsCopy[i] = nums[i];
+		swapNum[1] = insertionSort(numsCopy)[0];
+		for (int i = 0; i < nums.length; ++i) numsCopy[i] = nums[i];
+		swapNum[2] = selectionSort(numsCopy)[0];
+		int lowestNum = Integer.MAX_VALUE, lowestIndex = 0;
+		for (int i = 0; i < 3; ++i) {
+			if (swapNum[i] < lowestNum) { // check if sort produces least swaps
+				lowestNum = swapNum[i];
+				lowestIndex = i; // store index of optimal sort
+			}
+		}
+		return sortNames[lowestIndex];
 	}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static String leastIterations(double[] nums) {
+		int[] itNum = new int[3]; // stores number of iterations for bubble, insertion, selection sort in that order
+		String[] sortNames = {"Bubble", "Insertion", "Selection"}; // parallel array to store the names of each algorithm
+		double[] numsCopy = new double[nums.length];
+		for (int i = 0; i < nums.length; ++i) numsCopy[i] = nums[i];
+		itNum[0] = bubbleSort(numsCopy)[1];
+		for (int i = 0; i < nums.length; ++i) numsCopy[i] = nums[i];
+		itNum[1] = insertionSort(numsCopy)[1];
+		for (int i = 0; i < nums.length; ++i) numsCopy[i] = nums[i];
+		itNum[2] = selectionSort(numsCopy)[1];
+		int lowestNum = Integer.MAX_VALUE, lowestIndex = 0;
+		for (int i = 0; i < 3; ++i) {
+			if (itNum[i] < lowestNum) { // check if sort produces least swaps
+				lowestNum = itNum[i];
+				lowestIndex = i; // store index of optimal sort
+			}
+		}
+		return sortNames[lowestIndex];
 	}
 
 }
